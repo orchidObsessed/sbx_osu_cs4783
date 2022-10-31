4b765b6aad88105cfc696d9bfaeae0ae6be165ce
orchidObsessed <william.waddell0507@gmail.com>
2022-10-31 02:23:20 -0500

    Kinda-sorta finished q2, got one part of q1 finished

--- ass3/main.py
+++ ass3/main.py
@@ -4,30 +4,72 @@ from tensorflow import keras
 import numpy as np
 
 # Question 1
+# Load data (and specify dimensional arguments for reference later)
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+in_shape = (28, 28, 1) # 28x28x1 image
+
+x_train, x_test = np.expand_dims(x_train, -1), np.expand_dims(x_test, -1)
+y_train, y_test = keras.utils.to_categorical(y_train, 10), keras.utils.to_categorical(y_test, 10)
+print(f"Expected: {in_shape}\n Actual : {x_train.shape}")
+
+
+# Build models
+conv_model_increasing = keras.Sequential([keras.layers.Input(shape=in_shape),
+                                          keras.layers.Conv2D(filters=32, kernel_size=2, activation="relu"),
+                                          keras.layers.Conv2D(filters=64, kernel_size=2, activation="relu"),
+                                          keras.layers.Conv2D(filters=96, kernel_size=2, activation="relu"),
+                                          keras.layers.MaxPooling2D(pool_size=2, strides=2, padding="same"),
+                                          keras.layers.Conv2D(filters=128, kernel_size=2, activation="relu"),
+                                          keras.layers.Conv2D(filters=256, kernel_size=2, activation="relu"),
+                                          keras.layers.Conv2D(filters=384, kernel_size=2, activation="relu"),
+                                          keras.layers.MaxPooling2D(pool_size=2, strides=2, padding="same"),
+                                          keras.layers.Conv2D(filters=512, kernel_size=2, activation="relu"),
+                                          keras.layers.Conv2D(filters=1024, kernel_size=2, activation="relu"),
+                                          keras.layers.Conv2D(filters=1536, kernel_size=2, activation="relu"),
+                                          keras.layers.MaxPooling2D(pool_size=2, strides=2, padding="same"),
+                                          keras.layers.Flatten(),
+                                          keras.layers.Dense(10, activation="softmax")])
+
+conv_model_increasing.summary()
+conv_model_decreasing = None
+vae_model = None
+
+# Train models
+conv_model_increasing.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+conv_model_increasing.fit(x_train, y_train, epochs=10, validation_split=0.1)
+
+# Evaluate models
+eval_score = conv_model_increasing.evaluate(x_test, y_test, verbose=0)
+print(f"    Loss: {eval_score[0]}\nAccuracy: {eval_score[1]}")
+
+
 
 # Question 2
 # Load data (and specify dimensional arguments for reference later)
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-in_shape = (32, 32, 3) # 32x32 image, with depth of 3 (RGB color channels)
 
-# Build model
-lenet_model = keras.Sequential([keras.layers.Input(shape=in_shape),                             # ""Input"" layer (assuming this doesn't count towards 7-layer count for LeNet?)
-                                keras.layers.Conv2D(6, (5, 5), strides=1, activation="relu"),   # 6 kernels of 5x5, stride=1
-                                keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),            # 2x2 kernels, stride=2
-                                keras.layers.Conv2D(16, (5, 5), strides=1, activation="relu"),  # 16 kernels of 5x5, stride=1
-                                keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),            # 2x2 kernels, stride=2
-                                keras.layers.Conv2D(120, (5, 5), strides=1, activation="relu"), # 120 kernels of 5x5
-                                keras.layers.Dense(84, activation="relu"),                      # 84 fully connected fprop
-                                keras.layers.Dense(10, activation="softmax")])                  # 10 neurons for 10 classes, softmax for multiclass classification
-
-# Train model
-batch_size = 50
-epochs = 25
-lenet_model.compile(loss="categorical_crossentropy", optimizer="adam")
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-
-# Evaluate model (for funsies, since we already have test splits)
-eval_score = model.evaluate(x_test, y_test)
-print(f"  Loss  : {eval_score[0]}\nAccuracy: {eval_score[1]}")
+# (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+# in_shape = (32, 32, 3) # 32x32 image, with depth of 3 (RGB color channels)
+# print(f"Expected: {in_shape}\n Actual : {x_train.shape}")
+# y_train, y_test = keras.utils.to_categorical(y_train, 10), keras.utils.to_categorical(y_test, 10)
+#
+# # Build model
+# lenet_model = keras.Sequential([keras.layers.Input(shape=in_shape),                                                 # Keras input object
+#                                 keras.layers.Conv2D(filters=6, kernel_size=(5, 5), strides=1, activation="relu"),   # 6 kernels of 5x5, stride=1
+#                                 keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2),                                # 2x2 kernels, stride=2
+#                                 keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=1, activation="relu"),  # 16 kernels of 5x5, stride=1
+#                                 keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2),                                # 2x2 kernels, stride=2
+#                                 keras.layers.Conv2D(filters=120, kernel_size=(5, 5), strides=1, activation="relu"), # 120 kernels of 5x5
+#                                 keras.layers.Flatten(),                                                             # Also assuming the flattening layer doesn't count towards 7-layer count for LeNet, since it's not trainable anyways
+#                                 keras.layers.Dense(84, activation="relu"),                                          # 84 fully connected fprop
+#                                 keras.layers.Dense(10, activation="softmax")])                                      # 10 neurons for 10 classes, softmax for multiclass classification
+#
+# lenet_model.summary()
+# # Train model
+# lenet_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+# lenet_model.fit(x_train, y_train, epochs=25, validation_split=0.1)
+#
+# # Evaluate model (for funsies, since we already have test splits)
+# eval_score = lenet_model.evaluate(x_test, y_test, verbose=0)
+# print(f"    Loss: {eval_score[0]}\nAccuracy: {eval_score[1]}")
 
 # Question 3
